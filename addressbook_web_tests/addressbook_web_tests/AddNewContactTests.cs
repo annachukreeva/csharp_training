ff392304c75054be0f902258a752b2d7cf225b42
annachukreeva <ann-chukreeva@mail.ru>
2018-11-24 16:09:11 +0300

    Homework 5

---
+++ addressbook_web_tests/addressbook_web_tests/AddNewContactTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+
+namespace WebAddressbooktests
+{
+    [TestFixture]
+    public class AddNewContactsTests : TestBase
+    {
+         [Test]
+        public void AddNewContactTest()
+        {
+            GoToHomePage();
+            Login(new AccountData ("admin", "secret"));
+            PressAddNewContact();
+            FillTheDataAddNewContact(new ContactData("Anna", "Chukreeva"));
+            SubmitAddNewContact();
+            ReturnToHomePage();
+            Logout();
+        }
+    }
+}          
+          
+   
--- addressbook_web_tests/addressbook_web_tests/GroupRemovalTests.cs
+++ addressbook_web_tests/addressbook_web_tests/GroupRemovalTests.cs
@@ -3,41 +3,12 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 using NUnit.Framework;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.Support.UI;
 
 namespace WebAddressbooktests
 {
     [TestFixture]
-    public class DeleteGroupTests
+    public class DeleteGroupTests : TestBase
     {
-        private IWebDriver driver;
-        private StringBuilder verificationErrors;
-        private string baseURL;
-        private bool acceptNextAlert = true;
-
-        [SetUp]
-        public void SetupTest()
-        {
-            driver = new FirefoxDriver();
-            baseURL = "http://localhost/addressbook";
-            verificationErrors = new StringBuilder();
-        }
-
-        [TearDown]
-        public void TeardownTest()
-        {
-            try
-            {
-                driver.Quit();
-            }
-            catch (Exception)
-            {
-                // Ignore errors if unable to close the browser
-            }
-            Assert.AreEqual("", verificationErrors.ToString());
-        }
         [Test]
         public void DeleteGroupTest()
         {
@@ -47,88 +18,6 @@ namespace WebAddressbooktests
             SelectGroup(1);
             DeleteGroup();
             ReturnToGroupPage();
-        }
-        private void GoToHomePage()
-        {
-            driver.Navigate().GoToUrl(baseURL);
-        }
-        private void ReturnToGroupPage()
-        {
-            driver.FindElement(By.XPath("(//*[normalize-space(text()) and normalize-space(.)='Groups'])[1]/following::div[1]")).Click();
-            driver.FindElement(By.LinkText("group page")).Click();
-        }
-        private void Login(AccountData account)
-        {
-            driver.FindElement(By.Name("user")).Clear();
-            driver.FindElement(By.Name("user")).SendKeys(account.Username);
-            driver.FindElement(By.Name("pass")).Click();
-            driver.FindElement(By.Name("pass")).Clear();
-            driver.FindElement(By.Name("pass")).SendKeys(account.Password);
-            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
-            driver.FindElement(By.Id("content")).Click();
-        }
-        private void DeleteGroup()
-        {
-            driver.FindElement(By.Name("delete")).Click();
-        }
-
-        private void SelectGroup(int index)
-        {
-            driver.FindElement(By.XPath("(//input[@name='selected[]'])[" + index + "]")).Click();
-            driver.FindElement(By.XPath("//input[@name='selected[]']")).Click();
-        }
-
-        private void GoToGroupsPage()
-        {
-            driver.FindElement(By.LinkText("groups")).Click();
-        }
-
-        private bool IsElementPresent(By by)
-        {
-            try
-            {
-                driver.FindElement(by);
-                return true;
-            }
-            catch (NoSuchElementException)
-            {
-                return false;
-            }
-        }
-
-        private bool IsAlertPresent()
-        {
-            try
-            {
-                driver.SwitchTo().Alert();
-                return true;
-            }
-            catch (NoAlertPresentException)
-            {
-                return false;
-            }
-        }
-
-        private string CloseAlertAndGetItsText()
-        {
-            try
-            {
-                IAlert alert = driver.SwitchTo().Alert();
-                string alertText = alert.Text;
-                if (acceptNextAlert)
-                {
-                    alert.Accept();
-                }
-                else
-                {
-                    alert.Dismiss();
-                }
-                return alertText;
-            }
-            finally
-            {
-                acceptNextAlert = true;
-            }
-        }
-    }
+        }            
+     }
 }
