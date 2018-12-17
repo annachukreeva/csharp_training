169945d08e6e43b0a4383950250caf57dbafaff9
annachukreeva <ann-chukreeva@mail.ru>
2018-12-17 21:46:35 +0300

    Homework 14

--- addressbook_web_tests/addressbook-test-data-generators/Program.cs
+++ addressbook_web_tests/addressbook-test-data-generators/Program.cs
@@ -13,13 +13,14 @@ namespace addressbook_test_data_generators
 {
     class Program
     {
-        static void Main(string[] args)
+        static void MainGroups(string[] args)
         {
             int count = Convert.ToInt32(args[0]);
             StreamWriter writer = new StreamWriter(args[1]);
             string format = args[2];
 
             List<GroupData> groups = new List<GroupData>();
+          //  List<ContactData> contacts = new List<ContactData>();
             for (int i = 0; i < count; i++)
             {
                 groups.Add(new GroupData(TestBase.GenerateRandomString(10))
@@ -49,6 +50,44 @@ namespace addressbook_test_data_generators
             writer.Close();
         }
 
+        static void MainContacts(string[] args)
+        {
+            int count = Convert.ToInt32(args[0]);
+            StreamWriter writer = new StreamWriter(args[1]);
+            string format = args[2];
+
+            List<ContactData> contacts = new List<ContactData>();
+            for (int i = 0; i < count; i++)
+            {
+                contacts.Add(new ContactData(TestBase.GenerateRandomString(10))
+                {
+                    Firstname = TestBase.GenerateRandomString(100),
+                    Lastname = TestBase.GenerateRandomString(100)
+
+                });
+
+            }
+            if (format == "csv")
+            {
+                writeContactsToCsvFile(contacts, writer);
+            }
+            else if (format == "xml")
+            {
+                writeContactsToXmlFile(contacts, writer);
+            }
+            else if (format == "json")
+            {
+                writeContactsToJsonFile(contacts, writer);
+            }
+            else
+            {
+                System.Console.Out.Write("Unrecognized format" + format);
+            }
+            writer.Close();
+        }
+
+        
+        //Groups
         static void writeGroupsToCsvFile(List<GroupData> groups, StreamWriter writer)
         {
             foreach (GroupData group in groups)
@@ -58,7 +97,7 @@ namespace addressbook_test_data_generators
             }
 
         }
-
+    
         static void writeGroupsToXmlFile(List<GroupData> groups, StreamWriter writer)
         {
             new XmlSerializer(typeof(List<GroupData>)).Serialize(writer, groups);
@@ -67,6 +106,28 @@ namespace addressbook_test_data_generators
         static void writeGroupsToJsonFile(List<GroupData> groups, StreamWriter writer)
         {
             writer.Write(JsonConvert.SerializeObject(groups, Newtonsoft.Json.Formatting.Indented));
+        }
+
+
+        //Contacts
+        static void writeContactsToCsvFile(List<ContactData> contacts, StreamWriter writer)
+        {
+            foreach (ContactData contact in contacts)
+            {
+                writer.WriteLine(String.Format("${0},${1},${2}",
+                contact.Firstname, contact.Lastname));
+            }
+
+        }
+
+        static void writeContactsToXmlFile(List<ContactData> contacts, StreamWriter writer)
+        {
+            new XmlSerializer(typeof(List<ContactData>)).Serialize(writer, contacts);
+        }
+
+        static void writeContactsToJsonFile(List<ContactData> contacts, StreamWriter writer)
+        {
+            writer.Write(JsonConvert.SerializeObject(contacts, Newtonsoft.Json.Formatting.Indented));
         }
     }
 }
--- addressbook_web_tests/addressbook_web_tests/Tests/AddNewContactTests.cs
+++ addressbook_web_tests/addressbook_web_tests/Tests/AddNewContactTests.cs
@@ -3,6 +3,10 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
 using NUnit.Framework;
 
 namespace WebAddressbooktests
@@ -24,7 +28,23 @@ namespace WebAddressbooktests
             return contacts;
         }
 
-        [Test, TestCaseSource("RandomContactDataProvider")]
+        public static IEnumerable<ContactData> ContactDataFromXmlFile()
+        {
+
+            return (List<ContactData>)
+                new XmlSerializer(typeof(List<ContactData>))
+                .Deserialize(new StreamReader(@"contacts.xml"));
+        }
+
+        public static IEnumerable<ContactData> ContactDataFromJsonFile()
+        {
+            return JsonConvert.DeserializeObject<List<ContactData>>(
+                File.ReadAllText(@"contacts.json"));
+
+        }
+
+        // [Test, TestCaseSource("RandomContactDataProvider")]
+        [Test, TestCaseSource("ContactDataFromJsonFile")]
         public void AddNewContactTest(ContactData contact)
         {
            //ContactData contact = new ContactData("Anna");
